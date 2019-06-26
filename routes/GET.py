a19d595867f11d42c0965f99dc0cebc0d939613f
ErrorCode51 <43345236+ErrorCode51@users.noreply.github.com>
2019-06-26 22:30:07 +0200

    got rid of json soup

--- routes/GET.py
+++ routes/GET.py
@@ -1,28 +1,43 @@
-from flask import abort, Blueprint
+from flask import abort, Blueprint, jsonify
 import database.dbMain as dbMain
 import database.dbModels as dbModels
 import sqlalchemy
 
 getBP = Blueprint('getBP', __name__) # a blueprint to add all routes to, so the Flask instance can import them in one line of code
 
+def copy_without_keys(d, keys):
+    return {x: d[x] for x in d if x not in keys}
+
+
+def toJson(jsonDict):
+    jsonDict = copy_without_keys(jsonDict, ['_sa_instance_state'])
+    return jsonify(jsonDict)
+
+
+def getUserDict(user):
+    if not isinstance(user, dbModels.user):
+        raise AttributeError('Input is not a user object')
+    userDict = user.__dict__
+    userDict['profilePic'] = user.profilePic
+    userDict['contacts'] = (lambda contacts: None if not contacts else [c.id for c in contacts])(user.contacts)
+    userDict = copy_without_keys(userDict, ['_sa_instance_state'])
+    return userDict
+
+
 def genUserJson(user):
     if not isinstance(user, dbModels.user):
         raise AttributeError('Input is not a user object')
-    return ('{"id": ' + str(user.id)
-            + ', "userName": "' + user.userName
-            + '", "email": "' + user.email
-            + '", "passwordHash": "' + user.passwordHash
-            + '", "profilePic": ' + (lambda pp: 'null' if pp == None else ('"' + pp + '"'))(user.profilePic)
-            + ', "contacts": ' + (lambda contacts: 'null' if not contacts else '[' + ', '.join([str(c.id) for c in contacts]) + ']')(user.contacts)
-            + ' }')
+    userDict = getUserDict(user)
+    return toJson(userDict)
 
 
 @getBP.route('/user/<id>/', methods=['get'])
-def getUserByName(id):
+def getUserByID(id):
     data = dbMain.selectObjectById(dbModels.user, id)
     try:
-        return(genUserJson(data)), 200, {'Content-Type': 'application/json; charset=utf-8'}
-    except AttributeError:
+        return(genUserJson(data), 200)
+    except AttributeError as e:
+        print(e)
         abort(404)
 
 
@@ -30,37 +45,37 @@ def getUserByName(id):
 def getUserByEmail(email):
     data = dbMain.getUserByEmail(email)
     try:
-        return(genUserJson(data)), 200, {'Content-Type': 'application/json; charset=utf-8'}
+        return(genUserJson(data), 200)
     except AttributeError:
         abort(404)
 
 
 @getBP.route('/user/all/', methods=['get'])
 def getAllUsers():
-    users = dbMain.selectAllObjectByType(dbModels.user)
-    return '{"users": [' + ', '.join([genUserJson(u) for u in users]) + ']}', 200, {'Content-Type': 'application/json; charset=utf-8'}
+    try:
+        users = dbMain.selectAllObjectByType(dbModels.user)
+        return(toJson({'users': [getUserDict(u) for u in users]}))
+    except AttributeError as e:
+        print(e)
+        abort(404)
 
 
 @getBP.route('/user/username/<username>/', methods=['get'])
-def getUserById(username):
+def getUserByName(username):
     data = dbMain.getUserByUserName(username)
     try:
-        return(genUserJson(data)), 200, {'Content-Type': 'application/json; charset=utf-8'}
+        return(genUserJson(data)), 200
     except AttributeError:
         abort(404)
 
 
-@getBP.route('/post/<post_id>', methods=['get'])
+@getBP.route('/post/<post_id>/', methods=['get'])
 def getPostByID(post_id):
     data = dbMain.selectObjectById(dbModels.post, post_id)
     try:
-        return('{"id":' + str(data.id)
-               + ', "user_id": ' + str(data.user_id)
-               + ', "title": "' + data.title
-               + '", "message": "' + data.message
-               + '"}')\
-            , 200, {'Content-Type': 'application/json; charset=utf-8'}
-    except AttributeError:
+        return(toJson(data.__dict__), 200)
+    except AttributeError as e:
+        print(e)
         abort(404)
 
 
@@ -68,20 +83,11 @@ def getPostByID(post_id):
 def getProjectByID(id):
     print('/project/<id>/')
     project = dbMain.selectObjectById(dbModels.project, id)
+    projectDict = project.__dict__
+    projectDict['participants'] = (lambda participants: None if not participants else [p.id for p in participants])(project.participants)
     print(project)
     try:
-        json = '{"id": ' + str(project.id) \
-            + ', "ownerId": ' + str(project.ownerId) \
-            + ', "name": "' + project.name \
-            + '", "description": "' + project.description + '", '
-        if project.participants:
-            json += '"participants": [' \
-            + ', '.join([str(u.id) for u in project.participants]) \
-            + ']'
-        else:
-            json += '"participants": null'
-        json += '}'
-        return json, 200, {'Content-Type': 'application/json; charset=utf-8'}
+        return toJson(project.__dict__), 200
     except AttributeError as e:
         print(e)
         abort(404)
